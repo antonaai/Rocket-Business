de4df1174f9b868945184c1bcc0f7b394528b8d6
Antonio Aiello <theitaliancoder13@gmail.com>
2018-06-09 19:46:59 +0000

    Updated portfolio and added fonts

--- app.js
+++ app.js
@@ -10,7 +10,19 @@ app.get("/", function(req, res){
 
 app.get("/about", function(req, res){
     res.render("about.ejs");
-})
+});
+
+app.get("/portfolio", function(req, res){
+    res.render("portfolio.ejs");
+});
+
+app.get("/portfolio/imagegallery", function(req, res) {
+    res.render("./portfolio/imageGallery.ejs");
+});
+
+app.get("/portfolio/loginform", function(req, res) {
+    res.render("./portfolio/loginForm.ejs");
+});
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("SERVER ONLINE");
